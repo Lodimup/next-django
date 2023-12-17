b1c5886f11a8a06641f4b3abc5dbb0b8337d481c
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-12-17 22:32:31 +0700

    Update import paths for NextAuthProvider

--- frontend/app/app/(auth)/layout.tsx
+++ frontend/app/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { NextAuthProvider } from "../providers";
+import { NextAuthProvider } from "@/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
