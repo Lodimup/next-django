e7d5e9b9a9e2303a7b8a8d8c46a2990ace750428
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-12-17 22:35:54 +0700

    Add authentication session check

--- frontend/app/app/(auth)/login/page.tsx
+++ frontend/app/app/(auth)/login/page.tsx
@@ -2,13 +2,15 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 import { AuthBtnGroup } from "./_components/auth-btn-group";
+import { auth } from "@/auth";
 
 export const metadata: Metadata = {
   title: "Authentication",
   description: "Authentication forms built using the components.",
 };
 
-export default function AuthenticationPage() {
+export default async function Page() {
+  const session = await auth();
   return (
     <div className="container relative flex-col h-screen items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
@@ -39,10 +41,10 @@ export default function AuthenticationPage() {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Login or create an account
+              {!session ? "Login or create an account" : "Welcome back"}
             </h1>
             <p className="text-sm text-muted-foreground">
-              Use providers below to login or create an account.
+              {!session && "Use providers below to login or create an account."}
             </p>
           </div>
           <AuthBtnGroup />
